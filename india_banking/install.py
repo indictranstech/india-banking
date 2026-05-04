6d9377eade880f40b7d400c37634b6f84c34e065
Nilima <nilima.d@gmail.com>
2026-05-04 13:02:37 +0530

    Added default banks in the bank master

--- india_banking/install.py
+++ india_banking/install.py
@@ -21,11 +21,11 @@ def after_install():
 	create_property_setter()
 	toggle_reqd_for_reference_in_payment_order(False)
 	create_default_mode_of_transfers()
-	# create_default_payment_type()
+	create_default_payment_type()
 	create_default_workflow()
-	# create_default_bank()
-	# update_allowed_payment_doctypes()
-	# create_default_roles()
+	create_default_bank()
+	update_allowed_payment_doctypes()
+	create_default_roles()
 
 
 def make_custom_fields():
